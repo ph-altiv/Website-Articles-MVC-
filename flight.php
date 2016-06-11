167946fa0f5d877e8943e00fb4243984351c5dd1
Aleynikov Timofey <aleynikov-timofey@yandex.ru>
2016-06-11 14:12:27 +0300

    Setting router class

--- flight.php
+++ flight.php
@@ -1,5 +1,22 @@
 <?php
+header('Content-type: text/html; charset=utf-8');
 
-include "templates/common.php";
+include('classes/router.php');
+
+define('ENDL', '</br>');
+$site_path = realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
+
+try
+{
+    Router::setControllersDirectory($site_path . "controllers");
+}
+catch(Exception $e)
+{
+    echo $site_path . ENDL;
+    $ferr = fopen("log.txt", "a");
+    fwrite($ferr, $e->getMessage() . PHP_EOL);
+    fclose($ferr);
+    exit("Произошла ошибка");
+}
 
 ?>
