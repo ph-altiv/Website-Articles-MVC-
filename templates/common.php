1120fb7463f66d594e1ce1b6e21d459b12244a66
Aleynikov Timofey <aleynikov-timofey@yandex.ru>
2016-06-11 11:48:46 +0300

    Common blocks the site design

---
+++ templates/common.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="/../styles/common.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="wrapper">
+
+    <header class="header">
+        It's header
+    </header>
+
+    <div class="middle">
+
+        <div class="container">
+            <main class="content">
+                Menu
+            </main>
+        </div>
+
+        <aside class="left-sidebar">
+            Left Sidebar
+            <a href="#" class="switch_content">Статьи</a>
+            <a href="#" class="switch_content">Форма</a>
+        </aside>
+
+    </div>
+
+    <footer class="footer">
+        It's footer
+    </footer>
+
+</div>
+
+</body>
+</html>
