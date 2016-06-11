921a959b9f5c5dbdb52e566c08ff7d0ef5ca7d1a
Aleynikov Timofey <aleynikov-timofey@yandex.ru>
2016-06-11 14:00:13 +0300

    [Router] Setting controllers directory

--- classes/router.php
+++ classes/router.php
@@ -3,7 +3,17 @@
 // Для разбора запросов и выбора контролера
 class Router
 {
-    
+    private static $path;
+
+    // Установка пути к контролерам
+    static function setControllersDirectory($path)
+    {
+        $path = rtrim($path, '/\\');
+        $path .= DIRECTORY_SEPARATOR;
+        if (is_dir($path) == false)
+            throw new Exception ('Invalid controller path: `' . $path . '`');
+        self::$path = $path;
+    }
 }
 
 ?>
